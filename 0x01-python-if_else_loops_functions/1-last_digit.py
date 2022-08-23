b1a54400099c528b4a7bc40a2bd214ad82d4ad7b
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-08-23 12:39:20 -0700

    number 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if length > 5:
     string = "and is greater than 5"
 if length == 0:
     string = "and is 0"
-if length < 6 and != 0:
+if length < 6 and is not 0:
     string = "and is less than 6 and not 0"
 
 print(f"Last digit of {number} is {length} {string}")
