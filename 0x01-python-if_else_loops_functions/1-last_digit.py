6e9533e483c682d21479c1c60df872b17e3f4ae4
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-08-23 12:37:33 -0700

    number 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+length = number[-1]
+if length > 5:
+    string = "and is greater than 5"
+if length == 0:
+    string = "and is 0"
+if length < 6 and != 0:
+    string = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {length} {string}")
