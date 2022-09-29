f4a8c2cf754b08b51d32d675c298d0d84c542dae
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-09-28 20:38:55 -0700

    update 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,5 @@
 
 def read_file(filename=""):
     with open(filename, encoding='utf-8') as f:
-        for line in f:
-            read_data = f.read()
-            print(line, end="")
+        read_data = f.read()
     f.closed
